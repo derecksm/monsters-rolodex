649784ec18a39982d0af599663f83cb564730bb7
Dereck San Miguel <dereck.sanmiguel@iherb.com>
2020-10-20 16:57:44 -0700

    Updates

---
+++ precache-manifest.a1ff676acbef607919a567ee0895c303.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e6dc8e518964ebc7f99ed0bbc49c7ad",
+    "url": "/monsters-rolozed/index.html"
+  },
+  {
+    "revision": "2a07ad197dc8b555ff3f",
+    "url": "/monsters-rolozed/static/css/main.95b484f6.chunk.css"
+  },
+  {
+    "revision": "88b685495fee72b0273c",
+    "url": "/monsters-rolozed/static/js/2.2eaf0afe.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/monsters-rolozed/static/js/2.2eaf0afe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a07ad197dc8b555ff3f",
+    "url": "/monsters-rolozed/static/js/main.b39e3490.chunk.js"
+  },
+  {
+    "revision": "f9d24d36f52630232747",
+    "url": "/monsters-rolozed/static/js/runtime-main.1cc59b28.js"
+  }
+]);
